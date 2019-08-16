65282b81f892e0ecea16f9824e335c1bb627bf34
Derek Etman <dereketman@gmail.com>
2019-08-16 11:02:09 -0700

    Ugly but works

--- starwars/src/components/CharacterCard.js
+++ starwars/src/components/CharacterCard.js
@@ -3,7 +3,7 @@ import { Card } from 'semantic-ui-react';
 import styled from "styled-components";
 
 const StarH2 = styled.h2`
-color:white;
+color:aqua;
 `;
 
 const Styledcard = styled.div `
@@ -12,9 +12,10 @@ background-color: #d7c078;
 border: 1px solid #5f9fdd;
 width: 400px;
 margin: 2rem auto;
-
-
+padding: 2%;
+border-radius: 20px;
 `;
+
 
 const CharacterCard = props => {
 
@@ -22,13 +23,13 @@ const CharacterCard = props => {
         <Card className = "char-list" key={props.id}>
             <Styledcard>
                 <Card.Content>
-                    <Card.Header><StarH2> Name: {props.name}</StarH2></Card.Header>
+                    <Card.Header><StarH2>{props.name}</StarH2></Card.Header>
                     <Card.Meta><p>Birth Year: {props.birth_year}</p></Card.Meta>
                     <Card.Description>
                     <p>Gender: {props.gender}</p>     
                     <p>Height: {props.height}</p>
                     <p>Mass: {props.mass}</p>
-                    <p>Home: {props.home_world}</p>
+                    <button onClick="window.location.href='{props.home_world}'">Home: {props.home_world}</button>
                     </Card.Description>
                     {/* <Card.Description>
                         Films: {props}
